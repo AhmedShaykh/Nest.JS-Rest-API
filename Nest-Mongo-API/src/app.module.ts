739acef00aff170863958d88f79088425f2c8143
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-08-26 16:12:40 +0500

    Initial Commit

--- Nest-Mongo-API/src/app.module.ts
+++ Nest-Mongo-API/src/app.module.ts
@@ -1,18 +1,9 @@
-import "dotenv/config";
-import { BookModule } from "./book/book.module";
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URL),
-    BookModule,
-  ],
+  imports: [],
   controllers: [],
   providers: []
 })
+
 export class AppModule { };
--- Nest-Mongo-API/src/main.ts
+++ Nest-Mongo-API/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT);
+  await app.listen(8080);
 
 };
 
