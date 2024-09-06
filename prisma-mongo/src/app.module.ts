fb99e0be0f8f1dfbc41490aacb3775822367193f
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2024-09-06 18:13:51 +0500

    Final Commit

--- prisma-mongo/src/app.module.ts
+++ prisma-mongo/src/app.module.ts
@@ -1,8 +1,22 @@
+import { PrismaModule } from "./prisma/prisma.module";
+import { AuthModule } from "./auth/auth.module";
+import { BookModule } from "./book/book.module";
+import { UserModule } from "./user/user.module";
+import { ConfigModule } from "@nestjs/config";
 import { Module } from "@nestjs/common";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    BookModule,
+  ],
   controllers: [],
-  providers: []
+  providers: [],
+  exports: []
 })
 export class AppModule { };
